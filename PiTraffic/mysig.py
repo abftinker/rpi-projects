bf6ef1ec13192c06fa56d5b18fe45e9017545869
a64Exploiter <e.exploiter@gmail.com>
2019-09-28 03:55:05 +0300

    the code behavior is better

--- PiTraffic/mysig.py
+++ PiTraffic/mysig.py
@@ -9,35 +9,32 @@ pins = [11, 12, 13, 15, 16, 18, 22, 29, 31, 33, 36, 38, 40]
 GPIO.setup(pins, GPIO.OUT)
 
 
-Directions = {"SOUTH": {'RED': 11, 'YELLOW': 13, "GREEN": 15},
-              "WEST": {'RED': 16, 'YELLOW': 18, "GREEN": 22},
-              "NORTH": {'RED': 29, 'YELLOW': 31, "GREEN": 33},
-              "EAST": {'RED': 36, 'YELLOW': 38, "GREEN": 40}}
+#  RED, YELLOW, GREEN
+Directions = {"SOUTH": [11, 13, 15],
+              "WEST": [16, 18, 22],
+              "NORTH": [29, 31, 33],
+              "EAST": [36, 38, 40]}
+
+
+for i in Directions:
+    print("Direction: {}".format(i))
+    for s in Directions[i]:
+        sleep(1)
+        print("{0} is True".format(s))
+        GPIO.output(s, True)
+        sleep(2)
+        print("{0} is False".format(s))
+        GPIO.output(s, False)
+
+
+print("Cleaning GPIO pins")
+GPIO.cleanup()
 
 
 def close(signal, frame):
-    print("\nTurning off ...\n")
+    print("\nExiting PiTraffic\n")
     GPIO.cleanup()
     sys.exit(0)
 
 
 signal.signal(signal.SIGINT, close)
-
-
-def Rockit(d):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            print(k)
-            Rockit(v)
-        else:
-            sleep(1)
-            print("{0} is True".format(v))
-            GPIO.output(v, True)
-            sleep(2)
-            print("{0} is False".format(v))
-            GPIO.output(v, False)
-
-
-Rockit(Directions)
-print("Cleaning GPIO pins")
-GPIO.cleanup()
